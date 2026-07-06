723415ccdbb755b5473cc97bae044a3c6555b8e6
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-07-05 20:32:22 -0600

    Adding first test

--- cypress/e2e/auth/login.cy.ts
+++ cypress/e2e/auth/login.cy.ts
@@ -6,10 +6,12 @@ describe('login test suite', () => {
     beforeEach(() => {
         // Tu código de prueba aquí
         cy.visit('https://conduit.bondaracademy.com/');
+        loginPage.header.clickOnSignInTab();
     })
 
     it('should login successfully with valid credentials', () => {
-        console.log('Starting login test with valid credentials');
-        loginPage.header.clickOnSignInTab();
+        loginPage.fillEmail('Test');
+        loginPage.fillPassword('TestPassword');
+        loginPage.clickSignIn();
     })
 })
--- cypress/support/pages/login.page.ts
+++ cypress/support/pages/login.page.ts
@@ -1,7 +1,21 @@
 import { MainPage } from "./main.page";
 
 class LoginPage extends MainPage{
-    
+    private emailInput = () => cy.get('input[placeholder="Email"]');
+    private passwordInput = () => cy.get('input[placeholder="Password"]');
+    private signInButton = () => cy.get('button[type="submit"]');   
+
+    fillEmail(email: string) {
+        this.emailInput().type(email);
+    }
+
+    fillPassword(password: string) {
+        this.passwordInput().type(password);
+    }
+
+    clickSignIn() {
+        this.signInButton().click();
+    }
 }
 
 export const loginPage = new LoginPage();
