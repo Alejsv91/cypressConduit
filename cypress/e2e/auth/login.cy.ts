02256904bcda597b1dd5aa9cf6a3004f9289cb52
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-07-22 14:44:35 -0600

    Adding first test related with login

--- cypress/e2e/auth/login.cy.ts
+++ cypress/e2e/auth/login.cy.ts
@@ -1,29 +1,52 @@
-import { constants } from "node:buffer";
 import { loginPage } from "../../support/pages/login.page";
-import { debug, log } from "node:console";
+import { Credentials } from "../../types/credentials.interfaces";
 
 /// <reference types="cypress" />
 
 describe("login test suite", () => {
-  const credentials = { email: "", password: "" };
+  const credentials: Credentials = { email: "", password: "" };
   beforeEach(() => {
     cy.env(["EMAIL", "PASSWORD"]).then((env) => {
-        credentials.email = env.EMAIL;
-        credentials.password = env.PASSWORD;
+      credentials.email = env.EMAIL;
+      credentials.password = env.PASSWORD;
     });
     cy.visit("/");
     loginPage.header.getSignInTab().should("be.visible");
     loginPage.header.clickOnSignInTab();
+    cy.url().should("contain", "/login");
     loginPage.getEmailInput().should("be.visible");
     loginPage.getPasswordInput().should("be.visible");
-
-  });
+    loginPage.getSignInButton().should("be.visible");
+  })
 
   it("should login successfully with valid credentials", () => {
-    console.log(`Credentials: ${credentials.email}, ${credentials.password}`);
-    loginPage.getSignInButton().should("be.visible");
+    userExecuteLogin(credentials);
+    cy.url().should("not.include", "/login");
+  })
+
+  it("When user don't filled the email input", () => {
+    loginPage.fillPassword(credentials.password);
+    loginPage.getSignInButton().should("be.disabled");
+  })
+
+  it("When user don't filled the password input", () => {
+    loginPage.fillEmail(credentials.email);
+    loginPage.getSignInButton().should("be.disabled");
+  })
+
+  it("Unsuccessfull login with invalid credentials", () => {
+    credentials.password = "fakePassword123";
+    userExecuteLogin(credentials);
+    //Validate if error is visible
+    loginPage
+      .getLoginErrorMessage()
+      .should("be.visible")
+      .should("have.text", "email or password is invalid");
+  })
+
+  function userExecuteLogin(credentials: Credentials) {
     loginPage.fillEmail(credentials.email);
     loginPage.fillPassword(credentials.password);
     loginPage.clickSignIn();
-  });
+  }
 });
--- cypress/support/pages/login.page.ts
+++ cypress/support/pages/login.page.ts
@@ -3,7 +3,12 @@ import { MainPage } from "./main.page";
 class LoginPage extends MainPage{
     private emailInput = () => cy.get('input[placeholder="Email"]');
     private passwordInput = () => cy.get('input[placeholder="Password"]');
-    private signInButton = () => cy.get('button[type="submit"]');   
+    private signInButton = () => cy.get('button[type="submit"]'); 
+    private loginErrorMessage = () => cy.get('app-list-errors > ul > li')
+    
+    getLoginErrorMessage(){
+        return this.loginErrorMessage();
+    }
 
     getEmailInput() {
         return this.emailInput();
