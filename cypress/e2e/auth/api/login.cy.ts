eee7e2344317a7624204637abcb565b66f7b97ea
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-07-27 17:19:27 -0600

    Adding API testing for login

--- cypress/e2e/auth/api/login.cy.ts
+++ cypress/e2e/auth/api/login.cy.ts
@@ -2,6 +2,7 @@ import { APIEndpoints } from "../../../support/constants/api-endpoints";
 import { Credentials } from "../../../types/credentials.interfaces";
 import { User } from "../../../types/user.interfaces";
 import { realUser } from "../../../support/factories/userFactory";
+import { fakeCredentials } from "../../../support/factories/credentialsFactory";
 
 describe("Login test for Login", () => {
   let apiUrl: string;
@@ -18,31 +19,41 @@ describe("Login test for Login", () => {
     console.log(`${apiUrl}${APIEndpoints.LOGIN}`);
   });
 
-  it("When user add invalid credentials", () => {
-    
-  })
-
   it("Successfull Login", () => {
-    console.log(`${apiUrl}${APIEndpoints.LOGIN}`);
-    cy.request({
-      method: "POST",
-      url: `${apiUrl}${APIEndpoints.LOGIN}`,
-      body: {
-        user: {
-          email: credentials.email,
-          password: credentials.password,
-        },
-      },
-    }).then((response) => {
+    cy.loginByApi(credentials).then((response) => {
+      expect(response.status).to.eq(200);
       realUser().then((expectedUser) => {
         let userResponse: User = { ...response.body.user };
-        expect(response.status).to.eq(200);
         expect(userResponse.bio).to.eq(expectedUser.bio);
         expect(userResponse.email).to.eq(expectedUser.email);
         expect(userResponse.image).to.eq(expectedUser.image);
         expect(userResponse.token).to.not.NaN;
         expect(userResponse.username).to.eq(userResponse.username);
       });
+    });
+  });
+
+  it("When user  don't add password", () => {
+    credentials.password = "";
+    cy.loginByApi(credentials).then((response) => {
+      //Should be 422 Unprocessable
+      expect(response.status).to.equal(422);
+    });
+  });
+
+  it("when user don't add email", () => {
+    credentials.email = "";
+    credentials.password = "fakePassword";
+    cy.loginByApi(credentials).then((response) => {
+      expect(response.status).to.equal(422);
+    });
+  });
+
+  it("When user add invalid credentials", () => {
+    console.log(`This is the fake credentials ${fakeCredentials.email}`);
+    cy.loginByApi(fakeCredentials).then((response) => {
+      //Should be 403 forbidden
+      expect(response.status).to.equal(403);
     });
   });
 });
